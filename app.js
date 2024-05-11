4f1b75ae8308b46e97c9b9b5551d46244174e58e
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2024-05-11 22:29:10 +0800

    ini koneksinya mau :_ (#3)

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+import "./helpers/message-builder/device-polyfill";
+import { getPackageInfo } from "@zos/app";
+import * as ble from "@zos/ble";
+import { MessageBuilder } from "./helpers/message-builder/message";
+import { log } from "@zos/utils";
+
 App({
   globalData: {},
   onCreate(options) {
-    console.log('app on create invoke')
+    console.log("app on create invoke");
+
+    const { appId } = getPackageInfo();
+
+    const messageBuilder = new MessageBuilder({
+      appId,
+      appDevicePort: 20,
+      appSidePort: 0,
+      ble,
+    });
+
+    this.globalData.messageBuilder = messageBuilder;
+    messageBuilder.connect();
   },
 
   onDestroy(options) {
-    console.log('app on destroy invoke')
-  }
-})
+    console.log("app on destroy invoke");
+    this.globalData.messageBuilder &&
+      this.globalData.messageBuilder.disConnect();
+  },
+});
